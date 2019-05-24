5c124b8e4854b43a968a911a05e65de1c11f4eb3
kid999888 <kid999888@163.com>
2019-05-24 13:01:29 +0900

    外部ライブラリ修正

--- main.h
+++ main.h
@@ -28,6 +28,7 @@
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "d3dx11.lib")
 #pragma comment (lib, "dxgi.lib")
+#pragma comment (lib, "dxguid.lib")
 #pragma comment (lib, "d3dcompiler.lib")
 
 //=================================================================================================
